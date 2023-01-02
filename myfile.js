386ce269dff883f139b768852306f3f5841fcb52
TilekT <44701112+TilekT@users.noreply.github.com>
2023-01-03 00:26:45 +0600

    feat add

---
+++ myfile.js
@@ -0,0 +1,10 @@
+function getStringLength(value) {
+    return value.length;
+  }
+
+  console.log(getStringLength('Tootby'));
+
+  function concatenateStrings(value1, value2) {
+    return value1+value2;
+  }
+  console.log(concatenateStrings('t','y'));
